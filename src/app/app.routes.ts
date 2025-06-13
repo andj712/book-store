918cafe0b0441039c8745565fbb6f78dcec3ff59
andj712 <andj712@gmail.com>
2025-06-13 15:21:37 +0200

    login stranica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Login },
+  { path: 'home', component: Home },
+  { path: 'contact', component: Contact }
+];
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  email: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+
+  onLogin() {
+    if (this.email === 'admin' && this.password === 'admin') {
+      this.router.navigate(['/home']);
+    } else {
+      alert('Invalid credentials');
+    }
+  }
 }
