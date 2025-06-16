a6f4c350e776db5c62b97f16957e6ee5314aa3de
andj712 <andj712@gmail.com>
2025-06-16 21:44:56 +0200

    contact page

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import { Navbar } from '../../components/navbar/navbar';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Button } from '../../components/button/button';
+import { InputField } from '../../components/input-field/input-field';
+import { ShadowOnHover } from '../../directives/shadow-on-hover';
+import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-contact',
-  imports: [CommonModule,Navbar],
   templateUrl: './contact.html',
-  styleUrl: './contact.scss'
+  styleUrls: ['./contact.scss'],
+  imports: [CommonModule, FormsModule,Button,InputField,ShadowOnHover,Navbar],
+
 })
 export class Contact {
+  name: string = '';
+  email: string = '';
+  message: string = '';
+  formSubmitted = false;
 
+  submitForm() {
+    if (this.name && this.email && this.message) {
+      this.formSubmitted = true;
+      console.log('Poruka poslata:', {
+        ime: this.name,
+        email: this.email,
+        poruka: this.message
+      });
+
+      this.name = '';
+      this.email = '';
+      this.message = '';
+    }
+  }
 }
