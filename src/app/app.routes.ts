9639ccbdd7b55d0477d100296cc4b21e6c288457
andj712 <andj712@gmail.com>
2025-06-18 15:16:15 +0200

    sredjivanje

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
-import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
 import { Books } from './pages/books/books';
 
 
 export const routes: Routes = [
   { path: '', component: Login },
-  { path: 'home', component: Home },
   { path: 'contact', component: Contact },
 { path: 'books', component: Books }
 ];
